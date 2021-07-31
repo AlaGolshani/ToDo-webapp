480203842abd94e9e0d625f57dfd291eb35cbadb
ala.golshani2001@gmail.com <ala.golshani2001@gmail.com>
2021-07-31 22:16:15 +0430

    admin site updated

--- apps/todo/admin.py
+++ apps/todo/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from .models import Task, Category
 
-admin.site.register(Task)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
+    list_editable = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'priority', 'category', 'due_date', 'complete']
+    list_editable = ['title', 'priority', 'category', 'complete']
+    search_fields = ['title']
